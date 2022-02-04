85bb2d553375af5dd12d63c09bea49619d032652
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2022-02-03 23:01:09 -0800

    Create testingTests.java

---
+++ lib/testingTests.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class testingTests{
+    @Test 
+    public void multiplication(){
+        assertEquals(2, testing.multiply(2, 5));
+    }
+}
