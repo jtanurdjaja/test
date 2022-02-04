65c7aee4b71d7aec9daf93bee7d93652e89b9c1c
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2022-02-03 23:10:53 -0800

    Update testingTests.java

--- testingTests.java
+++ testingTests.java
@@ -1,9 +1,9 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class testingTests{
-    @Test 
-    public void multiplication(){
-        assertEquals(2, testing.multiply(2, 5));
+public class testingTests {
+    @Test
+    public void multiplication() {
+        assertEquals(2, testing.multiply(1, 5));
     }
 }
